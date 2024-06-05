3d847156231b4edba0ee2beeeebbe7044771bf1d
devam12 <devampanchasara12@gmail.com>
2024-06-05 19:03:21 +0530

    build configration

--- app.config.js
+++ app.config.js
@@ -2,7 +2,13 @@ module.exports = {
     name: 'charge-ev',
     scheme: 'charge-ev',
     version: '1.0.0',
+    android: {
+        package: "com.project.chargeev",
+    },
     extra: {
         clerkPublishableKey: process.env.CLERK_PUBLISHABLE_KEY,
+        eas: {
+            projectId: "36b13449-c9c6-417e-b560-c3295b38c2e2",
+        },
     },
 };
