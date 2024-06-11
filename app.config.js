913d2364fb3c0add88a679bd68dff34e0835795e
devam12 <devampanchasara12@gmail.com>
2024-06-11 15:11:48 +0530

    icon splash updated

--- app.config.js
+++ app.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-    name: 'charge-ev',
+    name: 'Charge EV',
     scheme: 'charge-ev',
     version: '1.0.0',
+    icon: "./assets/icon.png",
+    splash: {
+        image: "./assets/splash.png",
+        resizeMode: "contain",
+        backgroundColor: "#ffffff",
+        loadingIndicatorStyleExperimental: "large"
+    },
     android: {
         package: "com.project.chargeev",
         config: {
